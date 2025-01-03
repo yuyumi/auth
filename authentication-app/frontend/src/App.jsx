a302e8056922a97d70fed78650b369b17df9ac00
yuyumi <changyu2000@gmail.com>
2025-01-03 18:25:20 -0500

    Update App.jsx

--- authentication-app/frontend/src/App.jsx
+++ authentication-app/frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import QRGenerator from './QRGenerator';
 import QRScannerPage from './QRScannerPage';
 
